ee608707d3fe142727952c1b5c589fe7fff30a62
ZENBOOK\fimab <yafimb@zoara.com>
2019-04-02 17:48:27 +0300

    Max temp

--- max-temperatures.py
+++ max-temperatures.py
@@ -12,9 +12,9 @@ def parseLine(line):
 
 lines         = sc.textFile("file:///SparkCourse/1800.csv")
 parsedLines   = lines.map(parseLine)
-minTemps      = parsedLines.filter(lambda x: "TMIN" in x[1])
+minTemps      = parsedLines.filter(lambda x: "TMAX" in x[1])
 stationTemps  = minTemps.map(lambda x: (x[0], x[2]))
-minTemps      = stationTemps.reduceByKey(lambda x, y: min(x,y))
+minTemps      = stationTemps.reduceByKey(lambda x, y: max(x,y))
 results       = minTemps.collect();
 
 for result in results:
