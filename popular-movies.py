f504e8f17a7e39fa64c327e3d2da75899efd1356
ZENBOOK\fimab <yafimb@zoara.com>
2019-04-03 16:46:17 +0300

    most popular super hero

--- popular-movies.py
+++ popular-movies.py
@@ -3,14 +3,13 @@ from pyspark import SparkConf, SparkContext
 conf = SparkConf().setMaster("local").setAppName("PopularMovies")
 sc = SparkContext(conf = conf)
 
-lines = sc.textFile("file:///SparkCourse/ml-100k/u.data")
-movies = lines.map(lambda x: (int(x.split()[1]), 1))
+lines       = sc.textFile("file:///SparkCourse/ml-100k/u.data")
+movies      = lines.map(lambda x: (int(x.split()[1]), 1))
 movieCounts = movies.reduceByKey(lambda x, y: x + y)
 
-flipped = movieCounts.map( lambda xy: (xy[1],xy[0]) )
+flipped      = movieCounts.map( lambda xy: (xy[1],xy[0]) )
 sortedMovies = flipped.sortByKey()
-
-results = sortedMovies.collect()
+results      = sortedMovies.collect()
 
 for result in results:
     print(result)
